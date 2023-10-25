0c0a08d07531d76117526a4bbc20308426e419b9
wasif <wasifmd119@gmail.com>
2023-10-25 16:22:42 +0530

    model

---
+++ model/skln_elasticnet.py
@@ -0,0 +1,82 @@
+def train(in_alpha, in_l1_ratio):
+
+    def eval_metrics(actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
+
+    # warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
+    #  Assumes wine-quality.csv is located in the same folder as the notebook
+    wine_path = "data/raw/wine-quality.csv"
+    data = pd.read_csv(wine_path)
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    # Set default values if no alpha is provided
+    if float(in_alpha) is None:
+        alpha = 0.5
+    else:
+        alpha = float(in_alpha)
+
+    # Set default values if no l1_ratio is provided
+    if float(in_l1_ratio) is None:
+        l1_ratio = 0.5
+    else:
+        l1_ratio = float(in_l1_ratio)
+
+    # Useful for multiple runs (only doing one run in this sample notebook)    
+    with mlflow.start_run():
+        # Execute ElasticNet
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        # Evaluate Metrics
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        # Print out metrics
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        # Log parameter, metrics, and model to MLflow
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        mlflow.sklearn.log_model(lr, "model")
+
+if __name__=="main":
+    import os
+    import warnings
+    import sys
+    import pandas as pd
+    import numpy as np
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import ElasticNet
+    import logging
+    import mlflow
+    import mlflow.sklearn
+    mlflow.set_tracking_url("https://dagshub.com/wasif20875/mlopsdagshub.mlflow")
+    tracking_url=mlflow.get_tracking_url()
+    print(f"current tracking url in {tracking_url}")
+
+    logging.basicConfig(level=logging.WARN)
+    logger=logging.getLogger(_name_)
+
+    for i in np.range(0,5,0.1):
+        train(i,i)
